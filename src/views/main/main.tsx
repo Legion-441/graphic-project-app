f94e2e6413e19800cd66b99790d565291e39842f
BartoMajor <sanczez.43b@gmail.com>
2023-01-22 14:22:28 +0100

    Adding blog box-picture and grid

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom"
-import AppBar from "../../components/app-bar/app-bar"
+import { Outlet } from "react-router-dom";
+import AppBar from "../../components/app-bar/app-bar";
 
 const MainView: React.FC = () => {
-    return <>
-        <AppBar />
-        <Outlet />
-        <div>Footer</div>
+  return (
+    <>
+      <AppBar />
+      <Outlet />
+      <div></div>
     </>
-}
+  );
+};
 
-export default MainView
+export default MainView;
