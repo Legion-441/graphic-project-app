dd96e4a4b6e2039c2cbcfc9ae74836674bfa5f45
Legion-441 <piotrbartnicki1995@gmail.com>
2023-01-22 18:57:28 +0100

    adding margin to footer & filter select

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,10 +4,11 @@ const Footer: React.FC = () => {
   return <Box
   component="div"
   sx={{
+    Width: "100%",
+    marginTop: '30px',
     backgroundColor: '#078080',
     color: '#232323',
     width: "100%",
-    maxWidth: "100%",
     height: "50px",
     alignItems: 'center',
     display: 'flex',
